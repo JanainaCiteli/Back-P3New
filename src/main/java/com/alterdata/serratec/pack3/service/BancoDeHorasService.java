402106975b6cca69badf1badd440ceefc1cbc8bd
janainaciteli <12562399+JanianaCiteli@users.noreply.github.com>
2022-01-11 09:15:49 -0300

    first Commit

---
+++ src/main/java/com/alterdata/serratec/pack3/service/BancoDeHorasService.java
@@ -0,0 +1,35 @@
+/*public BancoDeHoras addHoras(BancoDeHoras banco) {
+
+		banco.setHoraDisponivel(0.0);
+		List <BancoDeHoras>  = bancoRepository.findAllByColaborador(banco.getColaborador().getIdColaborador());
+		
+		for (iterable_type iterable_element : iterable) {
+			
+		}
+		
+		for (Compensacao c : banco.getCompensacoes()) {
+
+			banco.setHoraDisponivel(banco.getHoraDisponivel() + c.getTotalCompensacao());
+
+		}
+		return bancoRepository.save(banco);
+	}*/
+
+	/*public BancoDeHoras inserir(BancoDeHoras banco) {
+		try {
+ 	    	Optional<BancoDeHoras> banco = bancoRepository.findById(banco.get().getIdBanco());
+ 	    	
+ 	    	if(time.isEmpty()) {
+ 	    		throw new NotFoundException("Objeto não encontrado");
+ 	    	}
+ 	    	colaborador.setTime(time.get());
+ 	    	
+ 	    	} catch (NotFoundException e) {
+ 	    		e.printStackTrace();
+ 	    	}
+		
+		addHoras(banco);
+		return bancoRepository.save(banco);
+	}*/
+
+	
